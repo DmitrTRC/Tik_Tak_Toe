0c0c346a8a2cbddc9bf106557ed0b3ad1a48b6f5
Dmitry <9984398@gmail.com>
2020-11-08 18:10:05 +0100

    Player class implementation. Started. Init : Done set_marker() : Done

--- tiktak.py
+++ tiktak.py
@@ -15,6 +15,30 @@ class Board:
         print(self.positions[1] + v_line + self.positions[2] + v_line + self.positions[3])
 
 
+class Player:
+    marker = ()
+
+    def __init__(self, name='Human', human_mode=True):
+        self.human_mode = human_mode
+        self.name = name
+        self.set_marker()
+
+    def set_marker(self):
+        try:
+            if self.human_mode:
+                while (tmp_marker := input('Enter your SIGN ( X or 0 ) -> ').upper()) not in ('0', 'X'):
+                    print('Only "X" or "0" possible. Try again...')
+                Player.marker = (tmp_marker, '0' if tmp_marker == 'X' else 'X')
+            else:
+                print(f'Computer SIGN is: {Player.marker[1]}\n')
+        except IndexError:
+            print(f'You have to call Human init before .')
+
+
 if __name__ == '__main__':
     board = Board()
+
+    human_player = Player(input('Enter your nick -> '))
+    ai_player = Player(human_mode=False)
+
     board.redraw()
